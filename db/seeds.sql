1ae19fc40821c1a03279438edf73d0f5828e828f
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-11 17:00:38 -0500

    adding sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Last of the Mo-Jicama Burger");
+INSERT INTO burgers (burger_name) VALUES ("TGI Fried Egg Burger");
+INSERT INTO burgers (burger_name) VALUES ("Pickle My Funny Bone Burger");
