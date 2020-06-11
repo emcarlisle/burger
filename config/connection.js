e8a0caa30a5bee5d0399b26e73715191db7593f9
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-11 17:20:58 -0500

    adding config files

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// MYSQL CONNECTION
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Untitled1229",
+    database: "burgers_db"
+});
+
+// CONNECTION
+connection.connect((err) => {
+    if (err) {
+        console.error('error connectin: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+// EXPORT CONNECTION
+module.exports = connection;
