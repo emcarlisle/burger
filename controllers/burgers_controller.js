47490db566798bbb4fe975342863bde270b4009d
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-23 14:53:02 -0500

    changing PUT to async await

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,32 +10,26 @@ const burger = require('../models/burger');
 router.get(async (req, res) => {
     const burgers = await burger.all();
     res.render('index', { burgers });
-})
+});
 
 
 // POST
 router.post(async (req, res) => {
     const newBurger = await burger.create([req.body.burger_name]);
     res.json({ id: newBurger.insertId });
-})
+});
 
 
 
 // PUT
-router.put('/api/burgers/:id', (req, res) => {
-  //update one burger so grab id
-  burger.updateOne(req.params.id, (result) => {
-    res.json(result);
-  });
+router.put(async (req, res) => {
+    const condition = 'id = ' + req.params.id;
+    const results = await burger.update(condition);
+    if (results.changedRows === 0) {
+        return res.status(404).end();
+    }
+    res.status(200).end();
 });
 
 
 module.exports = router;
-
-
-/* TODO:
-- Work on the handlebars for main, index, and partials
-- Complete server.js
-- public folder --> burgers.js
-- Go over the controller.js
-- models folder --> burger.js for burger object*/
