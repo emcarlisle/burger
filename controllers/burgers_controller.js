32d20830ce356654450488824faca68ffcc6842b
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-23 14:44:02 -0500

    changing GET as async await

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,35 +1,42 @@
 // IMPORT EXPRESS
 const express = require('express');
 const router = express.Router();
+
 // IMPORT burger
 const burger = require('../models/burger');
+
+
 // GET
-router.get('/', (req, res) => {
-    burger.selectAll(
-        (data) => {
-            let bgObject = {
-                burgers: data
-            };
-            // render
-            res.render('index', bgObject);
-        }
-    )
+router.get(async (req, res) => {
+    const burgers = await burger.all();
+    res.render('index', { burgers });
 })
 
 
-// POST 
+// POST
 router.post('/api/burgers', (req, res) => {
-    burger.insertOne([
-        req.body.name
-    ], (result) => {
-            res.json(result);
-    })
-})
+  burger.insertOne([req.body.name], (result) => {
+    res.json(result);
+  });
+});
+
+
 
 // PUT
-router.put('api/burgers/:id', (req, res) => {
-    //update one burger so grab id
-    burger.updateOne(req.params.id, (result) => {
-        res.json(result);
-    }) 
-})
+router.put('/api/burgers/:id', (req, res) => {
+  //update one burger so grab id
+  burger.updateOne(req.params.id, (result) => {
+    res.json(result);
+  });
+});
+
+
+module.exports = router;
+
+
+/* TODO:
+- Work on the handlebars for main, index, and partials
+- Complete server.js
+- public folder --> burgers.js
+- Go over the controller.js
+- models folder --> burger.js for burger object*/
