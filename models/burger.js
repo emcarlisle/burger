86dd24ec09034472bb48d06d2c7407a08a5773c2
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-23 14:54:30 -0500

    fixing orm functions

--- models/burger.js
+++ models/burger.js
@@ -2,10 +2,17 @@
 const orm = require('../config/orm');
 
 // create the code that will call the ORM functions using burger specific input for the ORM
-//const burger = // CRUD
+const burger = {
+    all() {
+        return orm.selectAll();
+    },
+    create(burger_name) {
+        return orm.insertOne(burger_name);
+    },
+    update(condition) {
+        return orm.updateOne(condition);
+    }
+}
 
 // EXPORT ORM
-module.exports = burgers_db;
-
-// refer back to cats.js 
-// fix handlebars file so it transfers over
+module.exports = burger;
